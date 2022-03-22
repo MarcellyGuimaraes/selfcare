25f65bc9561a99f708a0cab1996f47edc8b6ed67
IsisG13 <isissouzaguimaraes@gmail.com>
2022-03-22 17:57:05 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { AiFillStar, AiOutlineSearch, AiOutlineUser } from 'react-icons/ai'
+import { BsCart2 } from 'react-icons/bs'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <h2>
-          <Image alt="logo" src="/assets/estrela.png" width={20} height={20} /> selfcare
+        <h2 className={styles.logo}>
+          <AiFillStar /> selfcare
         </h2>
+
+        <div className={styles.menu}>
+          <AiOutlineSearch className={styles.icon} size="2rem" color="#5CA720" />
+          <AiOutlineUser className={styles.icon} size="2rem" color="#5CA720" />
+          <BsCart2 className={styles.icon} size="2rem" color="#5CA720" />
+        </div>
       </header>
+
       <img alt="banner" src="/assets/banner.png" className={styles.banner} />
     </div>
   )
